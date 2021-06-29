0957d0f24411eec9adbde3f57c593d5a8c60668a
payn33 <amugenp2@gmail.com>
2021-06-29 15:45:11 +0100

    refactors login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import apiService from "../../context/apiService";
 import { AppStateContext } from "../../context";
-import { useToasts } from 'react-toast-notifications'
+import { useToasts } from "react-toast-notifications";
 
 // components
 import FullScreenLoader from "../../components/fullScreenLoader";
@@ -10,21 +10,19 @@ import FullScreenLoader from "../../components/fullScreenLoader";
 import logo from "../../assets/img/logo.png";
 import "./login.styles.scss";
 
-
 const Login = () => {
   const { authService } = useContext(AppStateContext);
 
   const emptyCredentials = {
     email: "",
-    password: ""
-  }
+    password: "",
+  };
 
   const [userCredentials, setCredentials] = useState(emptyCredentials);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const history = useHistory();
-  const { addToast } = useToasts()
-
+  const { addToast } = useToasts();
 
   const { email, password } = userCredentials;
 
@@ -32,26 +30,28 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      if (!email || !password) return; // further validations can be done on the input 
+      if (!email || !password) return; // further validations can be done on the input
       setIsLoading(true);
       const response = await apiService.login({ email, password });
       const { data } = response.data;
       authService.loginUser(data); // save user to the app state and to session storage using the authservice
-      addToast('Login Successful', { appearance: 'success', autoDismiss: true, });
+      addToast("Login Successful", {
+        appearance: "success",
+        autoDismiss: true,
+      });
 
-      if (data.role !== 'waiter') {
-        history.push('/dashboard'); // Redirect to dashboard on successful login
+      if (data.role !== "waiter") {
+        history.push("/dashboard"); // Redirect to dashboard on successful login
       } else {
-        history.push('/dashboard/cartmenu')
+        history.push("/dashboard/cartmenu");
       }
-
     } catch (error) {
       const message = apiService.getErrorMessage(error); // the getErrorMessage is a helper function to get the exact messages from the server
-      setError(message)
-      addToast(message, { appearance: 'error', autoDismiss: true, });
+      setError(message);
+      addToast(message, { appearance: "error", autoDismiss: true });
       setIsLoading(false);
       setTimeout(() => {
-        setError('')
+        setError("");
       }, 5000);
     }
   };
@@ -65,15 +65,31 @@ const Login = () => {
   return (
     <>
       {isLoading ? <FullScreenLoader /> : <></>}
-      <form className="form-container">
+      <div className="form-container">
         <div className="form-content">
-          <img src={logo} alt="logo" className="form-logo mx-auto" />
-          <p>Log in to your account to continue.</p>
-
           <form onSubmit={handleSubmit} className="login-form">
+            <div className="form-logo">
+              <img src={logo} alt="logo" className="form-logo mx-auto" />
+              <p>Log in to your account to continue.</p>
+            </div>
+
             <div className="form-item">
               <label htmlFor="grid-password">Email</label>
-              <i className="fas fa-user"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-user"
+              >
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
               <input
                 type="email"
                 name="email"
@@ -87,7 +103,21 @@ const Login = () => {
 
             <div className="form-item">
               <label htmlFor="grid-password">Password</label>
-              <i className="fas fa-lock"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-lock"
+              >
+                <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+              </svg>{" "}
               <input
                 type="password"
                 name="password"
@@ -99,18 +129,15 @@ const Login = () => {
               />
             </div>
 
-            <small className="text-red-500 font-bold">
-              {error}
-            </small>
+            <small className="text-red-500 font-bold">{error}</small>
 
             <div className="btn-wrapper">
               <button
-                className="login-btn"
+                className="btn login-btn"
                 type="submit"
-                style={{ transition: "all .15s ease" }}
               >
                 Login
-            </button>
+              </button>
             </div>
 
             <div className="login-footer">
@@ -119,11 +146,9 @@ const Login = () => {
                 <Link to="/">Go back to homepage</Link>
               </div>
             </div>
-
           </form>
-
         </div>
-      </form>
+      </div>
     </>
   );
 };
