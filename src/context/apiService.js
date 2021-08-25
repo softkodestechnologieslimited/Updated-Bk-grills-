01c3578acdb13811c08472af669b21f8a45c3154
p_yn3 <66219025+payn33@users.noreply.github.com>
2021-08-25 19:06:30 +0100

    Update apiService.js

--- src/context/apiService.js
+++ src/context/apiService.js
@@ -46,7 +46,7 @@ export class ApiService {
 
   getUsers = async () => this.axiosTwo.get("/staffs");
 
-  getUserDetails = async () => this.axiosTwo.get("/user/profile", addAuthorizedHeaders())
+  getUserDetails = async () => this.axiosTwo.get("/user/profile/", addAuthorizedHeaders())
 
   updateUserDetails = async (payload) => this.axiosTwo.patch('/user/profile/', payload, addAuthorizedHeaders())
 
